0505aa5b4191f936bfebcb6aa73beb2c51e38a87
caberg <carl.aberg@volvocars.com>
2022-12-22 21:59:23 +0100

    Added nextjs frontend

--- src/main/java/com/todo/demo/TodoController.java
+++ src/main/java/com/todo/demo/TodoController.java
@@ -1,5 +1,6 @@
 package com.todo.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class TodoController {
     }
 
     @GetMapping("index")
+    @CrossOrigin(origins = "http://localhost:3010")
     public GetTodoOutput showTodo(GetTodoInput input) {
         GetTodoOutput result = new GetTodoOutput();
 
